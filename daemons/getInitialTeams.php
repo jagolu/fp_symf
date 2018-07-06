5a0d375e7be157203432769218ce4584e5bce8da
Javier Gómez Luzón <javo14795@gmail.com>
2018-07-06 02:52:43 +0200

    GetTeams (lack to insert on database and get images)

--- daemons/getInitialTeams.php
+++ daemons/getInitialTeams.php
@@ -1,22 +1,18 @@
 <?php
-    $html=file_get_contents('http://www.laliga.es/laliga-santander');
-    $dom = new DomDocument;
+    $url=file_get_contents('http://www.laliga.es/laliga-santander');
+    $html = new DomDocument;
+    $dom = new DOMDocument('1.0', 'iso-8859-1');
+    $teams = new DOMNodeList;
 
-    @$dom->loadHTML($html);
+    @$html->loadHTML($url);
 
+    $dom = $html->getElementById("equipos");
 
-    $doc = new DOMXpath($dom);
-    $doc->query("//*[@id='equipos']");
-    
-    
-    //echo $doc->saveHTML();
+    $teams = $dom->getElementsByTagName("a");
 
-    //var_dump(count($teams));
-
-    /*foreach ($teams as $team){
-        $result=var_dump(count($team->getElementByClassName('nombre')));
-        echo "hola";
-        echo $result;
-    }*/
-
+    for($i=0;$i<$teams->length;$i++){
+        $teamName = $teams->item($i)->firstChild->nodeValue;
+        echo $teamName.'</br>';
+        //Aqui habria que insertarlos en la BD
+    }
 ?>
