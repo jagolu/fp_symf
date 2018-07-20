9d38851001004cb039ef2637a26799c710a8934e
Javier Gómez Luzón <javo14795@gmail.com>
2018-07-20 18:05:18 +0200

    Fixing welcomePage

--- mainPage/src/Controller/LoginController.php
+++ mainPage/src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\BCryptPasswordEncoder;
+use Symfony\Component\HttpFoundation\Session\Session;
 use App\Entity\User;
 
 class LoginController extends Controller
@@ -14,21 +15,31 @@ class LoginController extends Controller
         $email = $_POST['email'];
         $nickname = $_POST['nickname'];
         $password = $_POST['password'];
+
+        $session = new Session();
+        $session->start();
         
-        
+        if(strlen($email)<5){
+            $session->getFlashBag()->add('warning', 'Ha habido un problema con tu registro');
+            return $this->redirectToRoute('index');
+        }
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return new Response("Your email is not valid");
+            $session->getFlashBag()->add('warning', 'Ha habido un problema con tu registro');
+            return $this->redirectToRoute('index');
         }
         //Comprobacion tipografia password
         if(strlen($password)<8 || strlen($password)>20){
-            return new Response("There has been an error with the length of the password");
+            $session->getFlashBag()->add('warning', 'Ha habido un problema con tu registro');
+            return $this->redirectToRoute('index');
         }
         //Comprobacion tipografia username
         if(strlen($nickname)<3 || strlen($nickname)>20){
-            return new Response("There has been an error with the length of the nickname");
+            $session->getFlashBag()->add('warning', 'Ha habido un problema con tu registro');
+            return $this->redirectToRoute('index');
         }
         if($where != 'newRoom' && $where != 'existingRoom'){
-            return new Response("There has been an error with the value of the parameter 'where'".$where);
+            $session->getFlashBag()->add('warning', 'Ha habido un problema con tu registro');
+            return $this->redirectToRoute('index');
         }
 
 
@@ -37,7 +48,8 @@ class LoginController extends Controller
         ->getRepository(User::class)
         ->findByEmail($email);
         if(count($users)!=0){
-            return new Response('The user already exists');
+            $session->getFlashBag()->add('warning', 'Esa direccion de correo ya esta registrada');
+            return $this->redirectToRoute('index');
         }
         else{
             $user = new User();
